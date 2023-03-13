edb86276ef3e3368baf2a1755a0657e1c385e94c
andrejfydorov <andrejfydorov@mail.ru>
2023-03-13 12:33:37 +0300

    added squaring.c

--- src/squaring.c
+++ src/squaring.c
@@ -2,15 +2,16 @@
 #define NMAX 10
 
 int input(int *a, int *n);
-void output(int *a, int n);
-void squaring(int *a, int n);
+void output(int *a, int *n);
+void squaring(int *a, int *n);
 
 int main()
 {
     int n, data[NMAX];
-    input(data, n);
-    squaring(data, n);
-    output(data, n);
+    if(input(data, &n) != -1) {
+        squaring(data, &n);
+        output(data, &n);
+    }
 
     return 0;
 }
@@ -20,18 +21,32 @@ int input(int *a, int *n)
     scanf("%d", n);
     for(int *p = a; p - a < *n; p++)
     {
-        scanf("%d", p);
+        if(scanf("%d", p) != 1) {
+            printf("n/a");
+            return -1;
+        }
+    }
+    return 0;
+}
+
+void output(int *a, int *n)
+{
+    for(int *p = a; p - a < *n; p++)
+    {
+        if((p - a) == (*n)-1) {
+            printf("%d", *p);
+        }else {
+            printf("%d ", *p);
+        }
     }
 }
 
-void output(int *a, int n)
+void squaring(int *a, int *n)
 {
-    //NOTHING
-}
-
-void squaring(int *a, int n)
-{
-    //NOTHING
+    for(int *p = a; p - a < *n; p++)
+    {
+        *p = *p * *p;
+    }
 }
 
 
